c00683af0cfcd4cbd1459c9fc27e7ff5a4d59680
KomalBabar14 <komalbabar2009@gmail.com>
2024-03-26 08:53:52 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import inquirer from 'inquirer';
+const message = await inquirer.prompt([
+    {
+        name: "name",
+        msg: "Enter your name:",
+        type: "input"
+    }
+]);
+console.log("Your name is", message.name);
